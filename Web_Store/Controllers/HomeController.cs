b62cee5edf5c6af5d047ede748de2582582a4adb
Станислав Васильев <Stanislav.V.Vasilyev@hotmail.com>
2020-12-01 04:16:21 +0300

    The First Controller

---
+++ Web_Store/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Web_Store.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IConfiguration _Configuration;
+        public HomeController(IConfiguration Configuration) => _Configuration = Configuration;
+        public IActionResult Index()
+        {
+            //return View();
+            return Content("First Action");
+        }
+
+        public IActionResult SecondAction() {
+            return Content(_Configuration["ControllerText"]);
+        }
+    }
+}
