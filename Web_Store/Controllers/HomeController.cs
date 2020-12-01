9b6a028510c2368eb786de9e1eafee7e00465ff1
Станислав Васильев <Stanislav.V.Vasilyev@hotmail.com>
2020-12-01 15:01:31 +0300

    AuxiliaryData is detached

--- Web_Store/Controllers/HomeController.cs
+++ Web_Store/Controllers/HomeController.cs
@@ -4,6 +4,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Web_Store.Data;
 using Web_Store.Models;
 
 namespace Web_Store.Controllers
@@ -11,13 +12,7 @@ namespace Web_Store.Controllers
     public class HomeController : Controller
     {
         private readonly IConfiguration _Configuration;
-
-        private static readonly List<Employee> _Employees = new()
-        {
-            new Employee { Id = 1, FirstName = "Тихон", LastName = "Вашков", Patronymic = "Фёдорович", Age = 24 },
-            new Employee { Id = 2, FirstName = "Аркадий", LastName = "Чинилов", Patronymic = "Михайлович", Age = 27 },
-            new Employee { Id = 3, FirstName = "Денис", LastName = "Ламов", Patronymic = "Степанович", Age = 26 }
-        };
+            
 
         public HomeController(IConfiguration Configuration) => _Configuration = Configuration;
         public IActionResult Index()
@@ -31,7 +26,7 @@ namespace Web_Store.Controllers
         }
 
         public IActionResult Employees() {
-            return View(model:_Employees);
+            return View(AuxiliaryData._Employees);
         }
     }
 }
---
+++ Web_Store/Data/AuxiliaryData.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Web_Store.Models;
+
+namespace Web_Store.Data
+{
+    public static class AuxiliaryData
+    {
+        public static readonly List<Employee> _Employees = new()
+        {
+            new Employee { Id = 1, FirstName = "Тихон", LastName = "Вашков", Patronymic = "Фёдорович", Age = 24 },
+            new Employee { Id = 2, FirstName = "Аркадий", LastName = "Чинилов", Patronymic = "Михайлович", Age = 27 },
+            new Employee { Id = 3, FirstName = "Денис", LastName = "Ламов", Patronymic = "Степанович", Age = 26 }
+        };
+    }
+}
